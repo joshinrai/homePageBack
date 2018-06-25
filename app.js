23f653160ab16c941f775c4fc72f0601a6b84eec
joshinrai <joshinrai@163.com>
2018-06-25 19:21:25 +0800

    提交代码
    提交代码

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+let cors = require('cors')
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,6 +43,15 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+// 处理node跨域
+app.use('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); //这个表示任意域名都可以访问，这样写不能携带cookie了。
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');//设置方法
+  if (req.method == 'OPTIONS') res.send(200); // 意思是，在正常的请求之前，会发送一个验证，是否可以请求。
+  else next()
 });
 
 app.listen(8888)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let cors = require('cors')
+let router = express.Router();
+let MongoClient = require('mongodb').MongoClient;
+let url = 'mongodb://127.0.0.1:27017/homePage'
+
+let resResult = 'balabala...'
+let corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200
+}
+
+MongoClient.connect(url, function (err, db) {
+	if (err) throw err
+	let homePage = db.db('homePage')
+	homePage.collection('users').find({}).toArray(function (error, result) {
+		if (error) throw error
+		resResult = result[0].name
+		console.log('result is:', result)
+	})
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/test', function(req, res, next) {
+  res.json({data: resResult})
 });
 
 module.exports = router;
